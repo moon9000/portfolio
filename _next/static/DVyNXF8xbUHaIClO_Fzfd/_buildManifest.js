e0328082c2aea44d29239c3a6dd32d0995b7abb6
moon9000 <56639157+moon9000@users.noreply.github.com>
2022-11-05 16:38:28 +0000

    Deploying to gh-pages from @ moon9000/portfolio@1dda979bc0ccbb6930a9c54312ceccd6f255668f 🚀

---
+++ _next/static/DVyNXF8xbUHaIClO_Fzfd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-191e6ed6e31c74fa.js"],"/_error":["static/chunks/pages/_error-ed5695eaedf8db48.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

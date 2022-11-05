500bde4806a8633fb713b9a47503565a852dc9ba
Sabri <sabri.abdennader@wandi.fr>
2022-11-05 16:58:41 +0100

    fix(next.config.js): add images loader akamai

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

6d442bef083db014560f0de505e4458336b30a89
Sabri <sabri.abdennader@wandi.fr>
2022-11-06 01:17:15 +0100

    feat(pages/_app.tsx): add IntlProvider and ChakraProviders to enable them globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+import en from "../lang/en.json";
+import fr from "../lang/fr.json";
+
+const messages = {
+  en,
+  fr,
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { locale } = useRouter();
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </IntlProvider>
+  );
 }

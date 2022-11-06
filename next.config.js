2b9cb602af2764ddcc8157193410d7a659432959
Sabri <sabri.abdennader@wandi.fr>
2022-11-06 01:12:10 +0100

    feat(next.config.js): add i18n and webpack raw loader

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      loader: "raw-loader",
+    });
+
+    // Important: return the modified config
+    return config;
+  },
   swcMinify: true,
   images: {
     loader: "akamai",
@@ -9,6 +18,11 @@ const nextConfig = {
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  i18n: {
+    locales: ["en", "fr"],
+    // The default locale you want to be used when visiting a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "fr",
+  },
 };
 
 module.exports = nextConfig;

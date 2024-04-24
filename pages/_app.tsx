278fb04d536a487899734907abe96ac45ae416e1
Sabri <sabri.abdennader@wandi.fr>
2024-04-24 20:51:19 +0200

    menu wrapper in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { IntlProvider } from "react-intl";
 import en from "../lang/en.json";
 import fr from "../lang/fr.json";
+import { MenuWrapper } from "../components/MenuWrapper";
 
 const messages = {
   en,
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
       messages={locale ? messages[locale === "fr" ? "fr" : "en"] : undefined}
     >
       <ChakraProvider>
+        <MenuWrapper />
         <Component {...pageProps} />
       </ChakraProvider>
     </IntlProvider>

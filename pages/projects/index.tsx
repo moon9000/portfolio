025d7160833431fad8c22ebd9c20886498e790b4
moon9000 <sebros02@gmail.com>
2024-05-25 15:44:13 +0200

    fix display issues, add tocfl-learning project

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,7 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../../styles/Home.module.css";
-import { MenuWrapper } from "../../components/MenuWrapper";
 import { useIntl } from "react-intl";
 
 export default function Projects() {
@@ -20,40 +17,40 @@ export default function Projects() {
   const stackJmusicDiscover = intl.formatMessage({
     id: "page.projects.jmusicDiscover.stack",
   });
+  const descriptionTocflLearning = intl.formatMessage({
+    id: "page.projects.tocflLearning.description",
+  });
+  const stackTocflLearning = intl.formatMessage({
+    id: "page.projects.tocflLearning.stack",
+  });
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>{title}</h1>
+        <h1 className={styles.titleProjects}>{title}</h1>
 
         <div className={styles.grid}>
+          <a href="https://jmusic-discover.vercel.app/" className={styles.card}>
+            <h2>J-Music Discover</h2>
+            <p>{descriptionJmusicDiscover}</p>
+            <p>--------------------------------</p>
+            <p>{stackJmusicDiscover}</p>
+          </a>
+
+          <a href="https://tocfl-learning.vercel.app/" className={styles.card}>
+            <h2>TOCFL Learning</h2>
+            <p>{descriptionTocflLearning}</p>
+            <p>--------------------------------</p>
+            <p>{stackTocflLearning}</p>
+          </a>
+          
           <a
             href="https://music-artist-searcher.netlify.app/"
             className={styles.card}
           >
             <h2>Music Artist Searcher</h2>
             <p>{descriptionMusicArtistSearcher}</p>
+            <p>--------------------------------</p>
             <p>{stackMusicArtistSearcher}</p>
-          </a>
-
-          <a href="https://jmusic-discover.vercel.app/" className={styles.card}>
-            <h2>J-Music Discover</h2>
-            <p>{descriptionJmusicDiscover}</p>
-            <p>{stackJmusicDiscover}</p>
-          </a>
-
-          <a href="/" className={styles.card}>
-            <h2>Coming soon...</h2>
-            <p></p>
-          </a>
-
-          <a
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Coming soon...</h2>
-            <p></p>
           </a>
         </div>
       </main>

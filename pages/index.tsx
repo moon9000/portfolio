996c790025e6e7379f82e7c04f9a9f6f16293fcc
moon9000 <sebros02@gmail.com>
2024-06-20 21:36:15 +0200

    add react-responsive dependency, use client side rendering to use breakpoint with alternative title for mobile mode, fix responsive display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { MenuWrapper } from "../components/MenuWrapper";
 import { useIntl } from "react-intl";
+import { useMediaQuery } from 'react-responsive'
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
   const intl = useIntl();
+  const isResponsive = useMediaQuery({ query: '(max-width: 600px)' })
 
-  const title = intl.formatMessage({ id: "page.home.title" });
+  const responsiveTItle = intl.formatMessage({ id: "page.home.responsiveTitle" });
+  const  title = intl.formatMessage({ id: "page.home.title" });
   const description = intl.formatMessage({
     id: "page.home.description",
   });
 
-  return (
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted && (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>{title}</h1>
+        <h1 className={styles.title}>{isResponsive ? responsiveTItle : title}</h1>
         <p className={styles.description}>{description}</p>
       </main>
     </div>
